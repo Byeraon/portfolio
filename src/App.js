5eb6a8d60b3ebc0406831df1b771747f38e8276e
Byeraon <nikitka.crasnov2013@mail.ru>
2021-04-25 13:50:21 +0300

    👾 Update 0.1 👾

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.App}>
+      <div className={s.whiteblock}><div className={s.textblock}>
+        <h1>NIKITA </h1>
+        <h1>KRASNOV</h1>
+        <h2>Frontend Developer</h2>
+      </div>
+      </div>
+
+
+      <div className={s.imageblock}></div>
     </div>
   );
 }
